76e7cb49569b60656bb296789bb085cbd2e6cf7b
gnas <89742084+sangnguyenthien@users.noreply.github.com>
2023-06-13 23:49:08 +0700

    Update Media.java

--- AimsProject/src/hust/soict/cs/aims/media/Media.java
+++ AimsProject/src/hust/soict/cs/aims/media/Media.java
@@ -1,6 +1,13 @@
 package AimsProject.src.hust.soict.cs.aims.media;
 
+import AimsProject.src.hust.soict.cs.MediaComparatorByCostTitle;
+import AimsProject.src.hust.soict.cs.MediaComparatorByTitleCost;
+
+import java.util.Comparator;
+
 public abstract class Media {
+    public static final Comparator<Media> COMPARE_BY_COST_TITLE= new MediaComparatorByCostTitle() ;
+    public static final Comparator<Media> COMPARE_BY_TITLE_COST= new MediaComparatorByTitleCost() ;
     protected int id;
     protected String title;
     protected String category;
